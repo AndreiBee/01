1bbf7d52aff18abb1b41830368dc310cf63c750b
AndreiBee <andrei-pchalavodau@seznam.cz>
2024-05-12 14:41:59 +0200

    FIrst change

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 #include <iostream>
 
-int main (int, char **) {
+int main(int, char **)
+{
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
+    std::cout << "Finish" << std::endl;
     return 0;
 }
